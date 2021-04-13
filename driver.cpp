8ca6bed31a6775bbc232ba4b3441a423b19be520
Turner Frazier <turner.frazier@cuw.edu>
2021-04-12 19:15:33 -0500

    first commit

---
+++ driver.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "BinaryTree.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    BinaryTree* bt = new BinaryTree();
+    bt->add(5);
+    bt->add(2);
+    bt->add(13);
+    bt->add(17);
+    cout << bt->getCount() << endl;
+    bt->visitInOrder();
+    cout<<""<<endl;
+    bt->visitPreOrder();
+    cout<<""<<endl;
+    bt->visitPostOrder();
+    cout<<""<<endl;
+    return 0;
+}
